a5c33ae2211045a1925680af7a82ff5cc392f1cd
ssar <ssar@example.com>
2021-10-25 10:49:38 +0900

    훈련/테스트 데이터 생성 및 시각화 완료

--- data/fish_data.py
+++ data/fish_data.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 bream_length1 = pd.read_csv("C:/fish/bream_length.csv")
 bream_length = bream_length1.to_numpy().flatten()
-print(bream_length)
+#print(bream_length)
 
 bream_weight1  = pd.read_csv("C:/fish/bream_weight.csv")
 bream_weight = bream_weight1.to_numpy().flatten()
@@ -14,7 +14,7 @@ bream_weight = bream_weight1.to_numpy().flatten()
 
 smelt_length1  = pd.read_csv("C:/fish/smelt_length.csv")
 smelt_length = smelt_length1.to_numpy().flatten()
-print(smelt_length)
+#print(smelt_length)
 
 smelt_weight1  = pd.read_csv("C:/fish/smelt_weight.csv")
 smelt_weight = smelt_weight1.to_numpy().flatten()
@@ -26,6 +26,25 @@ fish_length = np.concatenate((bream_length,smelt_length))
 fish_weight =  np.concatenate((bream_weight,smelt_weight))
 
 fish_data = np.column_stack((fish_length, fish_weight))
-print(fish_data)
+#print(fish_data)
 fish_target = np.concatenate((np.ones(35), np.zeros(14)))
-print(fish_target)
+#print(fish_target)
+
+index = np.arange(47) 
+np.random.shuffle(index)
+
+# 35
+train_input = fish_data[index[:35]] # 훈련 데이터 (모델)
+target_target = fish_target[index[:35]] # 타겟 데이터 (모델)
+# print(train_input)
+# print(target_target)
+
+# 14개
+test_input = fish_data[index[35:]] # 훈련 데이터 (검증)
+test_target = fish_target[index[35:]] # 타겟 데이터 (검증)
+
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(test_input[:,0], test_input[:,1])
+plt.xlabel("length")
+plt.ylabel("weight")
+plt.show()
